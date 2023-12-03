eb452855ff32d5c46862c825e506b73b3d266437
vira <viraivaskiv@gmail.com>
2023-12-03 23:59:54 +0200

    update index.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -7,23 +7,28 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY = axios.defaults.headers.common['x-api-key'];
 
 function fetchBreeds() {
-  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return fetch(`${BASE_URL}/breeds1?api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      throw new Error(error);
+    });
 }
-
 function fetchCatByBreed(breedId) {
   return fetch(
     `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch();
 }
 
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -2,44 +2,29 @@ import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
-
-//======================================
 const selector = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const divCatInfo = document.querySelector('.cat-info');
 const error = document.querySelector('.error');
 // Сховати loader, error і catInfoDiv
-loader.classList.replace('loader', 'is-hidden');
+loader.classList.add('is-hidden');
 error.classList.add('is-hidden');
 divCatInfo.classList.add('is-hidden');
-
 let arrayBreedsId = [];
-
-// завантажити породи при завантаженні сторінки
-fetchBreeds()
-  .then(data => {
-    // масив об'єктів для використання у SlimSelect
-    data.forEach(element => {
-      arrayBreedsId.push({ text: element.name, value: element.id });
-    });
-    // ініціалізація SlimSelect зі збереженими даними про породи
-    new SlimSelect({
-      select: selector,
-      data: arrayBreedsId,
-    });
-  })
-  .catch(onFetchError);
 // додати слухач подій на зміну вибору породи
 selector.addEventListener('change', onSelectBreed);
 function onSelectBreed(event) {
-  loader.classList.replace('is-hidden', 'loader');
+  const breedId = event.currentTarget.value;
+  if (!breedId) {
+    return;
+  }
+  loader.classList.remove('is-hidden');
   error.classList.add('is-hidden');
   divCatInfo.classList.add('is-hidden');
-  const breedId = event.currentTarget.value;
   fetchCatByBreed(breedId)
     .then(data => {
       // приховати loader, показати selector
-      loader.classList.replace('loader', 'is-hidden');
+      loader.classList.add('is-hidden');
       selector.classList.remove('is-hidden');
       // вдобразити інформацію про кота
       const { url, breeds } = data[0];
@@ -48,23 +33,43 @@ function onSelectBreed(event) {
     })
     .catch(onFetchError);
 }
+// завантажити породи при завантаженні сторінки
+fetchBreeds()
+  .then(data => {
+    arrayBreedsId.push({
+      placeholder: true,
+      text: data[0].name,
+      value: '',
+      disabled: true,
+    });
+    // масив об'єктів для використання у SlimSelect
+    data.forEach(element => {
+      arrayBreedsId.push({
+        text: element.name,
+        value: element.id,
+      });
+    });
+    new SlimSelect({
+      select: selector,
+      data: arrayBreedsId,
+    });
+  })
+  .catch(onFetchError);
 // функція для обробки помилок
 function onFetchError() {
-  // показати selector, приховати loader
   selector.classList.remove('is-hidden');
-  loader.classList.replace('loader', 'is-hidden');
+  loader.classList.add('is-hidden');
   // вивести повідомлення про помилку
   Notiflix.Notify.failure(
     'Oops! Something went wrong! Try reloading the page or select another cat breed!',
     {
-      position: 'center-center',
+      position: 'top-center',
       timeout: 5000,
       width: '400px',
       fontSize: '24px',
     }
   );
 }
-// завантажити та відобразити породи при завантаженні
 fetchBreeds();
 //!================
 //https://www.npmjs.com/package/slim-select
